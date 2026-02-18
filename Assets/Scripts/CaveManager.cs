0f13576b85282ddc98af0e1ec9de304657e6d88f
Sebastian Wernqvist <sebastian@wernqvist.eu>
2026-02-18 16:35:02 +0100

    Created scripts for Cave and CaveManager

---
+++ Assets/Scripts/CaveManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using UnityEngine;
+
+public enum ResourceType { Iron, Coal, Gold }
+
+public class CaveManager : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        ArrayList<Cave> caves = new ArrayList<Cave>;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
