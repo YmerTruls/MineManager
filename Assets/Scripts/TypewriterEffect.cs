3746696a3ec32e672150ad78d66597156d23b552
Sebastian Wernqvist <sebastian@wernqvist.eu>
2026-02-18 13:46:55 +0100

    Add TypewriterEffect
    Import TextMesh Pro examples, fonts, shaders, materials, scenes, and resources into Assets/TextMesh Pro, and add a new TypewriterEffect component (TypewriterEffect.cs) that reveals TMP text over time with configurable speed and interpunction delays. Includes related .meta entries for the new assets.

---
+++ Assets/Scripts/TypewriterEffect.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using TMPro;
+using UnityEngine;
+
+[RequireComponent(typeof(TMP_Text))]
+
+public class TypewriterEffect : MonoBehaviour
+{
+    private TMP_Text _textBox;
+
+    // For prototyping
+    [Header("Test String")]
+    [SerializeField] private string testText;
+
+    // Basic Typewriter Functionality
+    private int _currentVisibleCharacterIndex;
+    private Coroutine _typeWriterCoroutine;
+
+    private WaitForSeconds _simpleDelay;
+    private WaitForSeconds _interpunctionDelay;
+
+    [Header("Typewriter Settings")]
+    [SerializeField] private float charactersPerSecond = 20;
+    [SerializeField] private float interpunctionDelay = 0.5f;
+
+    private void Awake()
+    {
+        _textBox = GetComponent<TMP_Text>();
+
+        _simpleDelay = new WaitForSeconds(1 / charactersPerSecond);
+        _interpunctionDelay = new WaitForSeconds(interpunctionDelay);
+    }
+    void Start()
+    {
+        SetText(_textBox.text);
+    }
+
+    void Update()
+    {
+   
+    }
+
+    public void SetText(string text)
+    {
+        if (_typeWriterCoroutine != null)
+            StopCoroutine(_typeWriterCoroutine);
+
+        _textBox.text = text;
+        _textBox.ForceMeshUpdate();
+
+        _textBox.maxVisibleCharacters = 0;
+        _currentVisibleCharacterIndex = 0;
+
+        _typeWriterCoroutine = StartCoroutine(Typewriter());
+    }
+
+    private IEnumerator Typewriter()
+    {
+        TMP_TextInfo textInfo = _textBox.textInfo;
+
+        while (_currentVisibleCharacterIndex < textInfo.characterCount)
+        {
+            char character = textInfo.characterInfo[_currentVisibleCharacterIndex].character;
+
+            _textBox.maxVisibleCharacters++;
+
+            if (character == '?' || character == '.' || character == ',' ||
+                character == ';' || character == '!' || character == '-')
+            {
+                yield return _interpunctionDelay;
+            }
+            else
+            {
+                yield return _simpleDelay;
+            }
+
+            _currentVisibleCharacterIndex++;
+        }
+    }
+}
