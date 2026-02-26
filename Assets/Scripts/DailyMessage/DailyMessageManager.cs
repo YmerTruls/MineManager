9bba579b8bd6903435275dc895eefd9e29204623
Linda Bergevald <linda.bergevald@gmail.com>
2026-02-26 10:17:27 +0100

    Added a daily message
    Daily message is added yippie

---
+++ Assets/Scripts/DailyMessage/DailyMessageManager.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using TMPro;
+
+public class DailyMessageManager : MonoBehaviour
+{
+    public TMP_Text DailyMessageText;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SetDailyMessage(string message)
+    {
+        DailyMessageText.SetText(message);
+    }
+}
