954213c2520705de434da5eb450206fe2a2cdd83
David-Pixeln <kalmarkiwi03@gmail.com>
2026-03-09 13:47:05 +0100

    goody good good

--- Assets/Scripts/DailyMessage/TabController.cs
+++ Assets/Scripts/DailyMessage/TabController.cs
@@ -14,8 +14,12 @@ public class TabController : MonoBehaviour
         for (int i = 0; i < dailyMessageManager.GetDailyMessageCount(); i++)
         {
             AddTab(i);
+            tabList[i].setSelected(true);
+            if (i >= 1)
+            {
+                tabList[i-1].setSelected(false);
+            }
         }
-        tabList[0].setSelected(true);
     }
 
     public void AddTab(int pageIndex)
